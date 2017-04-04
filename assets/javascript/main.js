7897cf79828b106c7092878b12778476d9f08bef
karla nava <karla.nm8@gmail.com>
2017-04-04 16:42:16 +0000

    first commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,69 @@
+var celdasVacias = [];
+var vaciasLongitud = document.getElementsByClassName("vacio").length;
+
+var celdasBomba = [];
+var	bombaLongitud = document.getElementsByClassName("bomba").length;
+
+var celdasNumero1 = [];
+var numeroLongitud1 = document.getElementsByClassName("numero1").length;
+
+var celdasNumero2 = [];
+var numeroLongitud2 = document.getElementsByClassName("numero2").length;
+ 
+var contenedorTabla = document.getElementById("contenedorTablaBuscaminas");
+
+/Ciclos celdas vacias a color/
+for (var i = 0 ; i < vaciasLongitud; i++) {
+	celdasVacias[i] = document.getElementsByClassName("vacio")[i];
+}
+for (var i = 0 ; i < vaciasLongitud; i++) {
+   celdasVacias[i].addEventListener('click', cambiarAColor, false); 
+}
+
+/Ciclos celdas vacias a bomba/
+for (var i = 0 ; i < bombaLongitud; i++) {
+	celdasBomba[i] = document.getElementsByClassName("bomba")[i];
+}
+for (var i = 0 ; i < bombaLongitud; i++) {
+   celdasBomba[i].addEventListener('click', explotarBombas, false);
+
+
+}
+
+/Ciclos celdas vacias a numero1/
+for (var i = 0 ; i < numeroLongitud1; i++) {
+	celdasNumero1[i] = document.getElementsByClassName("numero1")[i];
+}
+for (var i = 0 ; i < numeroLongitud1; i++) {
+   celdasNumero1[i].addEventListener('click', revelarNumero_1, false); 
+}
+
+/Ciclos celdas vacias a numero2/
+for (var i = 0 ; i < numeroLongitud2; i++) {
+	celdasNumero2[i] = document.getElementsByClassName("numero2")[i];
+}
+for (var i = 0 ; i < numeroLongitud2; i++) {
+   celdasNumero2[i].addEventListener('click', revelarNumero_2, false); 
+}
+
+
+
+function cambiarAColor() {
+	this.style.backgroundColor = "red";
+}
+
+function explotarBombas() {
+	alert("Perdiste  :( ");
+
+	for (var i = 0; i < bombaLongitud; i++) {
+		celdasBomba[i].innerHTML = "☠";
+	}	
+}
+
+function revelarNumero_1 () {
+	this.inner Text = "1";
+}
+
+function revelarNumero_2 () {
+	this.innerText = "2";
+}
